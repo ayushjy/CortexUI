d02269eba79f451c0d4b6e9d9055e3ff18d57276
Kuldeep <kuldeep4545ek@gmail.com>
2024-05-21 16:48:38 +0530

    initi

--- src/components/Apply.js
+++ src/components/Apply.js
@@ -7,13 +7,13 @@ const Apply = () => {
         <div className='text-4xl font-medium text-gray-100 leading-relaxed sora w-1/2 text-center'>Apply On Any Section Or Enable For Whole Page</div>
         </div>
         <div className='flex gap-6 mt-20'>
-          <div className='h-full flex flex-col gap-6 px-5 py-8 w-1/2 bg-[linear-gradient(90deg,#F87516_0%,#5E11FF_100%)] rounded-3xl'>
+          <div className='h-full flex flex-col gap-6 px-5 py-8 w-1/2 bg-[linear-gradient(90deg,#130c24_50%,#1f1830_100%)] rounded-3xl'>
             <div className='text-2xl font-medium text-gray-100 sora'>Apply On Section</div>
             <div className='font-normal text-gray-400 text-lg'>Apply on section is a game-changer, offering an unparalleled way to manage applications directly from your website. </div>
             <div><img src="/img/section.png" alt="section-png"/></div>
           </div>
           <div className='flex pt-16 w-1/2 h-full'>
-          <div className='flex flex-col gap-6 px-5 py-8 bg-[linear-gradient(90deg,#F87516_0%,#5E11FF_100%)] rounded-3xl'>
+          <div className='flex flex-col gap-6 px-5 py-8 bg-[linear-gradient(90deg,#130c24_50%,#1f1830_100%)] rounded-3xl'>
             <div className='text-2xl font-medium text-gray-100 sora'>Apply On Section</div>
             <div className='font-normal text-gray-400 text-lg'>Apply on section is a game-changer, offering an unparalleled way to manage applications directly from your website. </div>
             <div><img src="/img/section.png" alt="section-png"/></div>
--- src/components/Features.js
+++ src/components/Features.js
@@ -8,21 +8,21 @@ const Features = () => {
         <div className='font-normal text-gray-400 flex justify-center w-1/2 text-center text-lg'>Whether you're a seasoned web designer or just starting out, Motion Art for Elementor seamlessly integrates with the Elementor platform, providing you with a seamless and intuitive experience.</div>
         </div>
         <div className='mt-24 flex gap-6'>
-            <div className='w-1/3 h-full px-6 py-10 bg-[linear-gradient(90deg,#F87516_0%,#5E11FF_100%)] rounded-3xl'>
+            <div className='w-1/3 h-full px-6 py-10 bg-[linear-gradient(0deg,#130c24_50%,#1f1830_100%)] rounded-3xl'>
                 <div className='-mx-8 -mt-8 mb-6'><img src="/img/lightweight.png" alt="lightweight"/></div>
                 <div className='flex flex-col gap-4 items-start'>
                     <div className='text-2xl font-medium text-gray-100 sora'>Light Weight</div>
                     <div className='font-normal text-gray-400 text-lg sora'>Motion Art for Elementor is designed to be lightweight.</div>
                 </div>
             </div>
-            <div className='w-1/3 h-full px-6 py-10 bg-[linear-gradient(90deg,#F87516_0%,#5E11FF_100%)] rounded-3xl'>
+            <div className='w-1/3 h-full px-6 py-10 bg-[linear-gradient(90deg,#130c24_50%,#1f1830_100%)] rounded-3xl'>
                 <div className='-mx-8 -mt-8 mb-6'><img src="/img/like.png" alt="like"/></div>
                 <div className='flex flex-col gap-4 items-start'>
                     <div className='text-2xl font-medium text-gray-100 sora'>100% Responsive</div>
                     <div className='font-normal text-gray-400 text-lg  sora'>Create a consistent visual experience across all devices..</div>
                 </div>
             </div>
-            <div className='w-1/3 h-full px-6 py-10 bg-[linear-gradient(90deg,#F87516_0%,#5E11FF_100%)] rounded-3xl'>
+            <div className='w-1/3 h-full px-6 py-10 bg-[linear-gradient(90deg,#130c24_50%,#1f1830_100%)] rounded-3xl'>
                 <div className='-mx-8 -mt-8 mb-6'><img src="/img/interface.png" alt="interface"/></div>
                 <div className='flex flex-col gap-4 items-start'>
                     <div className='text-2xl font-medium text-gray-100 sora'>User Friendly Interface</div>
